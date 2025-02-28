a4f7f4beee24f4a84a110cfeef9502fdb1c86019
Pedro Henrique De Oliveira Machado <senac.pedroholiveiramachado@gmail.com>
2025-02-27 21:54:51 -0300

    chore: fazendo a construção do formulário de IMC #4

--- MultApps.Windows/FrmCalculadoraIMC.cs
+++ MultApps.Windows/FrmCalculadoraIMC.cs
@@ -16,5 +16,19 @@ namespace MultApps.Windows
         {
             InitializeComponent();
         }
+
+        private void chkCrianca_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCrianca.ForeColor = Color.DarkOliveGreen;
+            chkAdulto.ForeColor = Color.Gray;
+            chkAdulto.Checked = false;
+        }
+
+        private void chkAdulto_CheckedChanged(object sender, EventArgs e)
+        {
+            chkAdulto.ForeColor = Color.DarkOliveGreen;
+            chkCrianca.ForeColor = Color.Gray;
+            chkCrianca.Checked = false;
+        }
     }
 }
