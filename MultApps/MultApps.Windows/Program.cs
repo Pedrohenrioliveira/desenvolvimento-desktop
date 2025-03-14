cab60bb240e0b6e97685346cf6c383f34b0cddd0
Pedro Henrique De Oliveira Machado <senac.pedroholiveiramachado@gmail.com>
2025-03-13 21:50:00 -0300

    feat: criamos o SplashScreen

--- MultApps/MultApps.Windows/Program.cs
+++ MultApps/MultApps.Windows/Program.cs
@@ -8,9 +8,6 @@ namespace MultApps.Windows
 {
     internal static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
         [STAThread]
         static void Main()
         {
