c678ebed52ea1d9f977e007944a6d7e4f1c04681
Pedro Henrique De Oliveira Machado <senac.pedroholiveiramachado@gmail.com>
2025-03-14 21:50:17 -0300

    feat: Iniciamos a verificação do calculo de aposentadoria.

---
+++ MultApps/MultApps.Windows/FrmAposentadoria.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MultApps.Windows
+{
+    partial class FrmAposentadoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
+            this.lblNascimento = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContribuicao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpNascimento
+            // 
+            this.dtpNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNascimento.Location = new System.Drawing.Point(50, 94);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(236, 20);
+            this.dtpNascimento.TabIndex = 1;
+            // 
+            // lblNascimento
+            // 
+            this.lblNascimento.AutoSize = true;
+            this.lblNascimento.Location = new System.Drawing.Point(47, 78);
+            this.lblNascimento.Name = "lblNascimento";
+            this.lblNascimento.Size = new System.Drawing.Size(164, 13);
+            this.lblNascimento.TabIndex = 2;
+            this.lblNascimento.Text = "Informe sua data de nascimento :";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(47, 129);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 3;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.cmbSexo.Location = new System.Drawing.Point(50, 143);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(236, 21);
+            this.cmbSexo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Informe seu sexo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tempo de contribuição :";
+            // 
+            // txtContribuicao
+            // 
+            this.txtContribuicao.Location = new System.Drawing.Point(50, 198);
+            this.txtContribuicao.Name = "txtContribuicao";
+            this.txtContribuicao.Size = new System.Drawing.Size(236, 20);
+            this.txtContribuicao.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(50, 235);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmAposentadoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(832, 479);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtContribuicao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lblNascimento);
+            this.Controls.Add(this.dtpNascimento);
+            this.Name = "FrmAposentadoria";
+            this.Text = "FrmAposentadoria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
+        private System.Windows.Forms.Label lblNascimento;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContribuicao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ MultApps/MultApps.Windows/FrmAposentadoria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class FrmAposentadoria : Form
+    {
+        public FrmAposentadoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string textoNascimento = txtNascimento.Text;
+            DateTime dataNascimento = DateTime.Parse(textoNascimento);
+
+            DateTime dataAtual = DateTime.Now;
+
+            if (cmbSexo.SelectedItem != null && idade >= 65)
+            {
+                if (cmbSexo.SelectedItem.ToString() == "Masculino")
+                {
+
+                    label3.Text = "Voce pode se aposentar";
+                }
+            }
+        }
+    }
+}
+
